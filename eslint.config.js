130b9c726fb210b84aa62bf2add1d7fe2dec2471
Wesley Rodio <97860676+WesleyRodio@users.noreply.github.com>
2025-04-10 09:39:12 -0300

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslintPlugin from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import pluginPrettier from "eslint-plugin-prettier/recommended"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+
+export default defineConfig([
+  {
+    // root: true,
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["**/*.config.js", ".history/**/*"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.node,
+    },
+    plugins: {
+      pluginPrettier,
+      eslint: eslintPlugin,
+    },
+    rules: {
+      ...eslintPlugin.configs.recommended.rules,
+      semi: ["error", "never"],
+      "prefer-const": "error",
+      "no-unused-vars": "warn",
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+    // extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  },
+  eslintConfigPrettier,
+  pluginPrettier,
+])
