6e4b443b3a1e74b58109a4ee7d3fa8773369e7e7
Wesley Rodio <97860676+WesleyRodio@users.noreply.github.com>
2025-04-10 09:43:54 -0300

    Upgrades in privacity

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-import eslintPlugin from "@eslint/js"
-import { defineConfig } from "eslint/config"
-import globals from "globals"
-import pluginPrettier from "eslint-plugin-prettier/recommended"
-import eslintConfigPrettier from "eslint-config-prettier/flat"
+import config from "./eslint.config.js"
 
-export default defineConfig([
-  {
-    // root: true,
-    files: ["**/*.{js,mjs,cjs}"],
-    ignores: ["**/*.config.js", ".history/**/*"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-      globals: globals.node,
-    },
-    plugins: {
-      pluginPrettier,
-      eslint: eslintPlugin,
-    },
-    rules: {
-      ...eslintPlugin.configs.recommended.rules,
-      semi: ["error", "never"],
-      "prefer-const": "error",
-      "no-unused-vars": "warn",
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-    // extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  },
-  eslintConfigPrettier,
-  pluginPrettier,
-])
+export default config
